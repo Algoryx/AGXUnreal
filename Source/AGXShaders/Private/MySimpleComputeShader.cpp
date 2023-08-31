a6e36f71ccb1f420ff11c169adfb0d15d9ce2a0b
Josef <josef.holmner@algoryx.com>
2023-08-31 15:21:24 +0200

    Increase thread count

--- Source/AGXShaders/Private/MySimpleComputeShader.cpp
+++ Source/AGXShaders/Private/MySimpleComputeShader.cpp
@@ -11,49 +11,55 @@
 #include "CanvasTypes.h"
 #include "MaterialShader.h"
 
-DECLARE_STATS_GROUP(TEXT("MySimpleComputeShader"), STATGROUP_MySimpleComputeShader, STATCAT_Advanced);
-DECLARE_CYCLE_STAT(TEXT("MySimpleComputeShader Execute"), STAT_MySimpleComputeShader_Execute, STATGROUP_MySimpleComputeShader);
+DECLARE_STATS_GROUP(
+	TEXT("MySimpleComputeShader"), STATGROUP_MySimpleComputeShader, STATCAT_Advanced);
+DECLARE_CYCLE_STAT(
+	TEXT("MySimpleComputeShader Execute"), STAT_MySimpleComputeShader_Execute,
+	STATGROUP_MySimpleComputeShader);
 
 // This class carries our parameter declarations and acts as the bridge between cpp and HLSL.
 class AGXSHADERS_API FMySimpleComputeShader : public FGlobalShader
 {
 public:
-	
 	DECLARE_GLOBAL_SHADER(FMySimpleComputeShader);
 	SHADER_USE_PARAMETER_STRUCT(FMySimpleComputeShader, FGlobalShader);
-	
-	
+
 	class FMySimpleComputeShader_Perm_TEST : SHADER_PERMUTATION_INT("TEST", 1);
-	using FPermutationDomain = TShaderPermutationDomain<
-		FMySimpleComputeShader_Perm_TEST
-	>;
+	using FPermutationDomain = TShaderPermutationDomain<FMySimpleComputeShader_Perm_TEST>;
 
 	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
-		/*
-		* Here's where you define one or more of the input parameters for your shader.
-		* Some examples:
-		*/
-		// SHADER_PARAMETER(uint32, MyUint32) // On the shader side: uint32 MyUint32;
-		// SHADER_PARAMETER(FVector3f, MyVector) // On the shader side: float3 MyVector;
+	/*
+	 * Here's where you define one or more of the input parameters for your shader.
+	 * Some examples:
+	 */
+	// SHADER_PARAMETER(uint32, MyUint32) // On the shader side: uint32 MyUint32;
+	// SHADER_PARAMETER(FVector3f, MyVector) // On the shader side: float3 MyVector;
 
-		// SHADER_PARAMETER_TEXTURE(Texture2D, MyTexture) // On the shader side: Texture2D<float4> MyTexture; (float4 should be whatever you expect each pixel in the texture to be, in this case float4(R,G,B,A) for 4 channels)
-		// SHADER_PARAMETER_SAMPLER(SamplerState, MyTextureSampler) // On the shader side: SamplerState MySampler; // CPP side: TStaticSamplerState<ESamplerFilter::SF_Bilinear>::GetRHI();
+	// SHADER_PARAMETER_TEXTURE(Texture2D, MyTexture) // On the shader side: Texture2D<float4>
+	// MyTexture; (float4 should be whatever you expect each pixel in the texture to be, in this
+	// case float4(R,G,B,A) for 4 channels) SHADER_PARAMETER_SAMPLER(SamplerState, MyTextureSampler)
+	// // On the shader side: SamplerState MySampler; // CPP side:
+	// TStaticSamplerState<ESamplerFilter::SF_Bilinear>::GetRHI();
 
-		// SHADER_PARAMETER_ARRAY(float, MyFloatArray, [3]) // On the shader side: float MyFloatArray[3];
+	// SHADER_PARAMETER_ARRAY(float, MyFloatArray, [3]) // On the shader side: float
+	// MyFloatArray[3];
 
-		// SHADER_PARAMETER_UAV(RWTexture2D<FVector4f>, MyTextureUAV) // On the shader side: RWTexture2D<float4> MyTextureUAV;
-		// SHADER_PARAMETER_UAV(RWStructuredBuffer<FMyCustomStruct>, MyCustomStructs) // On the shader side: RWStructuredBuffer<FMyCustomStruct> MyCustomStructs;
-		// SHADER_PARAMETER_UAV(RWBuffer<FMyCustomStruct>, MyCustomStructs) // On the shader side: RWBuffer<FMyCustomStruct> MyCustomStructs;
+	// SHADER_PARAMETER_UAV(RWTexture2D<FVector4f>, MyTextureUAV) // On the shader side:
+	// RWTexture2D<float4> MyTextureUAV; SHADER_PARAMETER_UAV(RWStructuredBuffer<FMyCustomStruct>,
+	// MyCustomStructs) // On the shader side: RWStructuredBuffer<FMyCustomStruct> MyCustomStructs;
+	// SHADER_PARAMETER_UAV(RWBuffer<FMyCustomStruct>, MyCustomStructs) // On the shader side:
+	// RWBuffer<FMyCustomStruct> MyCustomStructs;
 
-		// SHADER_PARAMETER_SRV(StructuredBuffer<FMyCustomStruct>, MyCustomStructs) // On the shader side: StructuredBuffer<FMyCustomStruct> MyCustomStructs;
-		// SHADER_PARAMETER_SRV(Buffer<FMyCustomStruct>, MyCustomStructs) // On the shader side: Buffer<FMyCustomStruct> MyCustomStructs;
-		// SHADER_PARAMETER_SRV(Texture2D<FVector4f>, MyReadOnlyTexture) // On the shader side: Texture2D<float4> MyReadOnlyTexture;
+	// SHADER_PARAMETER_SRV(StructuredBuffer<FMyCustomStruct>, MyCustomStructs) // On the shader
+	// side: StructuredBuffer<FMyCustomStruct> MyCustomStructs;
+	// SHADER_PARAMETER_SRV(Buffer<FMyCustomStruct>, MyCustomStructs) // On the shader side:
+	// Buffer<FMyCustomStruct> MyCustomStructs; SHADER_PARAMETER_SRV(Texture2D<FVector4f>,
+	// MyReadOnlyTexture) // On the shader side: Texture2D<float4> MyReadOnlyTexture;
 
-		// SHADER_PARAMETER_STRUCT_REF(FMyCustomStruct, MyCustomStruct)
+	// SHADER_PARAMETER_STRUCT_REF(FMyCustomStruct, MyCustomStruct)
 
-		SHADER_PARAMETER_RDG_BUFFER_UAV(RWBuffer<FVector4f>, Output)
-		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D, RenderTarget)
-		
+	SHADER_PARAMETER_RDG_BUFFER_UAV(RWBuffer<FVector4f>, Output)
+	SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D, RenderTarget)
 
 	END_SHADER_PARAMETER_STRUCT()
 
@@ -61,40 +67,46 @@ public:
 	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
 	{
 		const FPermutationDomain PermutationVector(Parameters.PermutationId);
-		
+
 		return true;
 	}
 
-	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)
+	static void ModifyCompilationEnvironment(
+		const FGlobalShaderPermutationParameters& Parameters,
+		FShaderCompilerEnvironment& OutEnvironment)
 	{
 		FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
 
 		const FPermutationDomain PermutationVector(Parameters.PermutationId);
 
 		/*
-		* Here you define constants that can be used statically in the shader code.
-		* Example:
-		*/
+		 * Here you define constants that can be used statically in the shader code.
+		 * Example:
+		 */
 		// OutEnvironment.SetDefine(TEXT("MY_CUSTOM_CONST"), TEXT("1"));
 
 		/*
-		* These defines are used in the thread count section of our shader
-		*/
-		OutEnvironment.SetDefine(TEXT("THREADS_X"), NUM_THREADS_MySimpleComputeShader_X);
-		OutEnvironment.SetDefine(TEXT("THREADS_Y"), NUM_THREADS_MySimpleComputeShader_Y);
-		OutEnvironment.SetDefine(TEXT("THREADS_Z"), NUM_THREADS_MySimpleComputeShader_Z);
+		 * These defines are used in the thread count section of our shader
+		 */
+		OutEnvironment.SetDefine(TEXT("THREADS_XY"), NUM_THREADS_MySimpleComputeShader_XY);
 
-		// This shader must support typed UAV load and we are testing if it is supported at runtime using RHIIsTypedUAVLoadSupported
-		//OutEnvironment.CompilerFlags.Add(CFLAG_AllowTypedUAVLoads);
+		// This shader must support typed UAV load and we are testing if it is supported at runtime
+		// using RHIIsTypedUAVLoadSupported
+		// OutEnvironment.CompilerFlags.Add(CFLAG_AllowTypedUAVLoads);
 
-		// FForwardLightingParameters::ModifyCompilationEnvironment(Parameters.Platform, OutEnvironment);
+		// FForwardLightingParameters::ModifyCompilationEnvironment(Parameters.Platform,
+		// OutEnvironment);
 	}
+
 private:
 };
 
 // This will tell the engine to create the shader and where the shader entry point is.
-//                            ShaderType                            ShaderPath                     Shader function name    Type
-IMPLEMENT_GLOBAL_SHADER(FMySimpleComputeShader, "/AGXShadersShaders/MySimpleComputeShader.usf", "MySimpleComputeShader", SF_Compute);
+//                            ShaderType                            ShaderPath Shader function name
+//                            Type
+IMPLEMENT_GLOBAL_SHADER(
+	FMySimpleComputeShader, "/AGXShadersShaders/MySimpleComputeShader.usf", "MySimpleComputeShader",
+	SF_Compute);
 
 void FMySimpleComputeShaderInterface::DispatchRenderThread(
 	FRHICommandListImmediate& RHICmdList, FMySimpleComputeShaderDispatchParams Params,
@@ -107,19 +119,21 @@ void FMySimpleComputeShaderInterface::DispatchRenderThread(
 		DECLARE_GPU_STAT(MySimpleComputeShader)
 		RDG_EVENT_SCOPE(GraphBuilder, "MySimpleComputeShader");
 		RDG_GPU_STAT_SCOPE(GraphBuilder, MySimpleComputeShader);
-		
+
 		typename FMySimpleComputeShader::FPermutationDomain PermutationVector;
-		
+
 		// Add any static permutation options here
 		// PermutationVector.Set<FMySimpleComputeShader::FMyPermutationName>(12345);
 
-		TShaderMapRef<FMySimpleComputeShader> ComputeShader(GetGlobalShaderMap(GMaxRHIFeatureLevel), PermutationVector);
-		
+		TShaderMapRef<FMySimpleComputeShader> ComputeShader(
+			GetGlobalShaderMap(GMaxRHIFeatureLevel), PermutationVector);
 
 		bool bIsShaderValid = ComputeShader.IsValid();
 
-		if (bIsShaderValid) {
-			FMySimpleComputeShader::FParameters* PassParameters = GraphBuilder.AllocParameters<FMySimpleComputeShader::FParameters>();
+		if (bIsShaderValid)
+		{
+			FMySimpleComputeShader::FParameters* PassParameters =
+				GraphBuilder.AllocParameters<FMySimpleComputeShader::FParameters>();
 
 			FRDGBufferRef OutputBuffer = GraphBuilder.CreateBuffer(
 				FRDGBufferDesc::CreateBufferDesc(sizeof(FVector4f), Params.Width * Params.Height),
@@ -128,9 +142,14 @@ void FMySimpleComputeShaderInterface::DispatchRenderThread(
 			PassParameters->Output =
 				GraphBuilder.CreateUAV(FRDGBufferUAVDesc(OutputBuffer, PF_A32B32G32R32F));
 
-			FRDGTextureDesc Desc(FRDGTextureDesc::Create2D(Params.RenderTarget->GetSizeXY(), PF_R32_FLOAT, FClearValueBinding::White,TexCreate_RenderTargetable | TexCreate_ShaderResource | TexCreate_UAV));
-			FRDGTextureRef TmpTexture =GraphBuilder.CreateTexture(Desc, TEXT("ExampleComputeShader_TempTexture"));
-			FRDGTextureRef TargetTexture = RegisterExternalTexture(GraphBuilder, Params.RenderTarget->GetRenderTargetTexture(),TEXT("ExampleComputeShader_RT"));
+			FRDGTextureDesc Desc(FRDGTextureDesc::Create2D(
+				Params.RenderTarget->GetSizeXY(), PF_R32_FLOAT, FClearValueBinding::White,
+				TexCreate_RenderTargetable | TexCreate_ShaderResource | TexCreate_UAV));
+			FRDGTextureRef TmpTexture =
+				GraphBuilder.CreateTexture(Desc, TEXT("ExampleComputeShader_TempTexture"));
+			FRDGTextureRef TargetTexture = RegisterExternalTexture(
+				GraphBuilder, Params.RenderTarget->GetRenderTargetTexture(),
+				TEXT("ExampleComputeShader_RT"));
 			PassParameters->RenderTarget = GraphBuilder.CreateUAV(TmpTexture);
 
 			if (TargetTexture->Desc.Format == PF_R32_FLOAT)
@@ -150,59 +169,58 @@ void FMySimpleComputeShaderInterface::DispatchRenderThread(
 #endif
 			}
 
-
-			auto GroupCount = FComputeShaderUtils::GetGroupCount(FIntVector(Params.Width, Params.Height, 1), 1);
+			auto GroupCount = FComputeShaderUtils::GetGroupCount(
+				FIntVector(Params.Width, Params.Height, 1), NUM_THREADS_MySimpleComputeShader_XY);
 
 			GraphBuilder.AddPass(
-				RDG_EVENT_NAME("ExecuteMySimpleComputeShader"),
-				PassParameters,
+				RDG_EVENT_NAME("ExecuteMySimpleComputeShader"), PassParameters,
 				ERDGPassFlags::AsyncCompute,
-				[&PassParameters, ComputeShader, GroupCount](FRHIComputeCommandList& RHICmdList)
-			{
-				FComputeShaderUtils::Dispatch(RHICmdList, ComputeShader, *PassParameters, GroupCount);
-			});
+				[&PassParameters, ComputeShader, GroupCount](FRHIComputeCommandList& RHICmdList) {
+					FComputeShaderUtils::Dispatch(
+						RHICmdList, ComputeShader, *PassParameters, GroupCount);
+				});
 
-
-			
-			FRHIGPUBufferReadback* GPUBufferReadback = new FRHIGPUBufferReadback(TEXT("ExecuteMySimpleComputeShaderOutput"));
+			FRHIGPUBufferReadback* GPUBufferReadback =
+				new FRHIGPUBufferReadback(TEXT("ExecuteMySimpleComputeShaderOutput"));
 			AddEnqueueCopyPass(GraphBuilder, GPUBufferReadback, OutputBuffer, 0u);
 
-
-
-			auto RunnerFunc = [GPUBufferReadback, AsyncCallback](auto&& RunnerFunc, int Width, int Height) -> void
+			auto RunnerFunc = [GPUBufferReadback, AsyncCallback](
+								  auto&& RunnerFunc, int Width, int Height) -> void
 			{
-				if (GPUBufferReadback->IsReady()) {
-					
+				if (GPUBufferReadback->IsReady())
+				{
 					FVector4f* Buffer = (FVector4f*) GPUBufferReadback->Lock(1);
 					TArray<FVector4f> OutVal;
 
 					for (int i = 0; i < Width * Height; i++)
 						OutVal.Add(Buffer[i]);
-					
+
 					GPUBufferReadback->Unlock();
 
-					AsyncTask(ENamedThreads::GameThread, [AsyncCallback, OutVal]() {
-						AsyncCallback(OutVal);
-					});
+					AsyncTask(
+						ENamedThreads::GameThread,
+						[AsyncCallback, OutVal]() { AsyncCallback(OutVal); });
 
 					delete GPUBufferReadback;
-				} else {
-					AsyncTask(ENamedThreads::ActualRenderingThread, [RunnerFunc, Width, Height]() {
-						RunnerFunc(RunnerFunc, Width, Height);
-					});
+				}
+				else
+				{
+					AsyncTask(
+						ENamedThreads::ActualRenderingThread,
+						[RunnerFunc, Width, Height]() { RunnerFunc(RunnerFunc, Width, Height); });
 				}
 			};
 
-
 			const int Width = Params.Width;
 			const int Height = Params.Height;
-			AsyncTask(ENamedThreads::ActualRenderingThread, [RunnerFunc, Width, Height]() {
-				RunnerFunc(RunnerFunc, Width, Height);
-			});
-			
-		} else {
-			// We silently exit here as we don't want to crash the game if the shader is not found or has an error.
-			
+			AsyncTask(
+				ENamedThreads::ActualRenderingThread,
+				[RunnerFunc, Width, Height]() { RunnerFunc(RunnerFunc, Width, Height); });
+		}
+		else
+		{
+			// We silently exit here as we don't want to crash the game if the shader is not found
+			// or has an error.
 		}
 	}
 
