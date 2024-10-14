ef5404603a1d3847859cb262522d3cfb97dd5baa
Douglas Nordwall <douglas.nordwall@gmail.com>
2024-10-14 09:54:36 +0200

    add AGX_MovableTerrainComponent class

---
+++ Source/AGXUnreal/Private/Terrain/AGX_MovableTerrainComponent.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Terrain/AGX_MovableTerrainComponent.h"
+
---
+++ Source/AGXUnreal/Public/Terrain/AGX_MovableTerrainComponent.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProceduralMeshComponent.h"
+#include "AGX_MovableTerrainComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = "AGX_Terrain", meta = (BlueprintSpawnableComponent))
+class AGXUNREAL_API UAGX_MovableTerrainComponent : public UProceduralMeshComponent
+{
+	GENERATED_BODY()
+	
+};
