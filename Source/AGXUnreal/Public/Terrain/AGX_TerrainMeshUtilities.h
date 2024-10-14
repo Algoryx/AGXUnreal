c2a53dcec36a90cdae267802c6c554067e3b1049
Douglas Nordwall <douglas.nordwall@gmail.com>
2024-10-14 10:40:45 +0200

    add AGX_TerrainMeshUtilities class

---
+++ Source/AGXUnreal/Public/Terrain/AGX_TerrainMeshUtilities.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AGX_TerrainMeshUtilities.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGXUNREAL_API UAGX_TerrainMeshUtilities : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
