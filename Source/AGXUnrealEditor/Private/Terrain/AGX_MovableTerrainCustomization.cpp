6aaaa24842a06a7204cc10f1ecb6ab2ad0632046
Douglas Nordwall <douglas.nordwall@gmail.com>
2025-01-16 13:59:41 +0100

    add MovableTerrain Component Customization class

---
+++ Source/AGXUnrealEditor/Private/Terrain/AGX_MovableTerrainCustomization.cpp
@@ -0,0 +1,89 @@
+// Copyright 2024, Algoryx Simulation AB.
+
+#include "Terrain/AGX_MovableTerrainCustomization.h"
+
+// AGX Dynamics for Unreal includes.
+#include "Utilities/AGX_EditorUtilities.h"
+
+// Unreal Engine includes.
+#include "DetailCategoryBuilder.h"
+#include "DetailLayoutBuilder.h"
+#include "DetailWidgetRow.h"
+
+TSharedRef<IDetailCustomization> FAGX_MovableTerrainCustomization::MakeInstance()
+{
+	return MakeShareable(new FAGX_MovableTerrainCustomization);
+}
+void FAGX_MovableTerrainCustomization::CustomizeDetails(IDetailLayoutBuilder& InDetailBuilder)
+{
+	DetailBuilder = &InDetailBuilder;
+
+
+	IDetailCategoryBuilder& EditorCategory = DetailBuilder->EditCategory("AGX Editor");
+	// Retrieve the UObject associated with the details panel
+	TArray<TWeakObjectPtr<UObject>> ObjectsBeingCustomized;
+	DetailBuilder->GetObjectsBeingCustomized(ObjectsBeingCustomized);
+
+	if (ObjectsBeingCustomized.Num() > 0)
+	{
+		// Assuming a single component is being customized
+		if (UAGX_MovableTerrainComponent* Component =
+				Cast<UAGX_MovableTerrainComponent>(ObjectsBeingCustomized[0]))
+		{
+			EditorCategory.AddCustomRow(FText::FromString("RebuildMesh"))
+				.NameContent()[SNew(STextBlock)
+								   .Text(FText::FromString("Rebuild Mesh"))
+								   .Font(IDetailLayoutBuilder::GetDetailFont())]
+				.ValueContent()[SNew(SButton)
+									.Text(FText::FromString("RebuildMesh"))
+									.OnClicked_Lambda(
+										[Component]() -> FReply
+										{
+											// Call the ForceRebuildMesh function on the component
+											Component->RebuildEditorMesh();
+											return FReply::Handled();
+										})];
+		}
+	}
+
+	DetailBuilder->SortCategories(
+	[](const TMap<FName, IDetailCategoryBuilder*>& CategoryMap)
+	{
+		for (const TPair<FName, IDetailCategoryBuilder*>& Pair : CategoryMap)
+		{
+			int32 SortOrder = Pair.Value->GetSortOrder();
+			const FName CategoryName = Pair.Key;
+
+			if (CategoryName == "AGX Editor")
+			{
+				SortOrder += 1;
+			}
+			else if (CategoryName == "AGX Movable Terrain")
+			{
+				SortOrder += 4;
+			}
+			else if (CategoryName == "AGX Terrain")
+			{
+				SortOrder += 3;
+			}
+			else if (CategoryName == "AGX Terrain Rendering")
+			{
+				SortOrder += 4;
+			}
+			else
+			{
+				const int32 ValueSortOrder = Pair.Value->GetSortOrder();
+				if (ValueSortOrder >= SortOrder && ValueSortOrder < SortOrder + 10)
+				{
+					SortOrder += 10;
+				}
+				else
+				{
+					continue;
+				}
+			}
+
+			Pair.Value->SetSortOrder(SortOrder);
+		}
+	});
+}
