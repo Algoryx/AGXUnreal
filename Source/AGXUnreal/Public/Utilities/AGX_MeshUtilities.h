56ae04c31d8e7bc8c8cefcd36259156dfc3a82ef
Martin Nilsson <martin.nilsson@algoryx.com>
2025-09-22 15:14:31 +0200

    Initial steps towards supporting multiple Render Data vertex data layouts when creating Static Mesh assets

--- Source/AGXUnreal/Public/Utilities/AGX_MeshUtilities.h
+++ Source/AGXUnreal/Public/Utilities/AGX_MeshUtilities.h
@@ -350,7 +350,7 @@ public:
 	 * Also adds a SimpleCollision (Box primitive) to the created Static Mesh.
 	 */
 	static UStaticMesh* CreateStaticMesh(
-		const TArray<FVector3f>& Vertices, const TArray<uint32>& Triangles,
+		const TArray<FVector3f>& Positions, const TArray<uint32>& Triangles,
 		const TArray<FVector3f>& Normals, const TArray<FVector2D>& UVs,
 		const TArray<FVector3f>& Tangents, const FString& Name, UObject& Outer,
 		UMaterialInterface* Material);
