738549d1e669919d1876ced561883bdab4857400
Josef <josef.holmner@algoryx.com>
2025-11-06 09:30:26 +0100

    Update allocation strategy

--- Source/AGXUnrealShaders/Private/ParticleUpsamplingDataHandler.cpp
+++ Source/AGXUnrealShaders/Private/ParticleUpsamplingDataHandler.cpp
@@ -126,7 +126,7 @@ void FParticleUpsamplingBuffers::UpdateHashTableBuffers(
 		ActiveVoxelsTableOccupancy.SafeRelease();
 
 		// Create new, larger buffers.
-		ActiveVoxelsCapacity *= 2;
+		ActiveVoxelsCapacity = FMath::RoundUpToPowerOfTwo(ElementCount * 2.0);
 
 		ActiveVoxelIndices = InitSRVBuffer<FIntVector4>(
 			RHICmdList, TEXT("ActiveVoxelIndices"), ActiveVoxelsCapacity);
