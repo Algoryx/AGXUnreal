9aa828d383a2406d1ddd92fda81b6bc5ee843cac
Josef <josef.holmner@algoryx.com>
2026-08-17 13:59:02 +0200

    Setup rendering pipelines for stacking rendering operations

--- Source/AGXUnreal/Private/Sensors/AGX_CameraSensorComponent.cpp
+++ Source/AGXUnreal/Private/Sensors/AGX_CameraSensorComponent.cpp
@@ -5,19 +5,95 @@
 // AGX Dynamics for Unreal includes.
 #include "AGX_Check.h"
 #include "AGX_LogCategory.h"
+#include "AGX_PropertyChangedDispatcher.h"
 #include "Sensors/AGX_CameraBackend.h"
 #include "Sensors/AGX_SensorEnvironmentSubsystem.h"
 #include "Sensors/CameraBarrier.h"
 #include "Utilities/AGX_StringUtilities.h"
 
 // Unreal Engine includes.
+#include "CanvasItem.h"
 #include "Components/SceneCaptureComponent2D.h"
+#include "Engine/Canvas.h"
+#include "Engine/TextureRenderTarget2D.h"
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
+#include "Kismet/KismetRenderingLibrary.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "Materials/MaterialInterface.h"
 
 UAGX_CameraSensorComponent::UAGX_CameraSensorComponent()
 {
 	NativeBarrier.Reset(new FCameraBarrier());
+}
+
+void UAGX_CameraSensorComponent::SetResolution(FIntPoint InResolution)
+{
+	if (!IsResolutionValid(InResolution))
+	{
+		UE_LOG(
+			LogAGX, Warning,
+			TEXT("Camera Sensor Component '%s' in '%s' received invalid Resolution '%s'. "
+				 "Resolution will not be changed."),
+			*GetName(), *GetLabelSafe(GetOwner()), *InResolution.ToString());
+		return;
+	}
+
+	Resolution = InResolution;
+	SetupRenderPasses();
+}
+
+void UAGX_CameraSensorComponent::SetMaterialInputTextureParameterName(FName InParameterName)
+{
+	MaterialInputTextureParameterName = InParameterName;
+	SetupRenderPasses();
+}
+
+void UAGX_CameraSensorComponent::AddMaterialPass(UMaterialInterface* Material)
+{
+	MaterialPasses.Add(Material);
+	SetupRenderPasses();
+}
+
+bool UAGX_CameraSensorComponent::SetMaterialPass(int32 Index, UMaterialInterface* Material)
+{
+	if (!MaterialPasses.IsValidIndex(Index))
+		return false;
+
+	MaterialPasses[Index] = Material;
+	SetupRenderPasses();
+	return true;
+}
+
+bool UAGX_CameraSensorComponent::RemoveMaterialPass(UMaterialInterface* Material)
+{
+	const int32 NumRemoved =
+		MaterialPasses.RemoveAll(
+			[Material](const TObjectPtr<UMaterialInterface>& ExistingMaterial)
+			{
+				return ExistingMaterial.Get() == Material;
+			});
+	if (NumRemoved == 0)
+		return false;
+
+	SetupRenderPasses();
+	return true;
+}
+
+bool UAGX_CameraSensorComponent::RemoveMaterialPassAt(int32 Index)
+{
+	if (!MaterialPasses.IsValidIndex(Index))
+		return false;
+
+	MaterialPasses.RemoveAt(Index);
+	SetupRenderPasses();
+	return true;
+}
+
+void UAGX_CameraSensorComponent::ClearMaterialPasses()
+{
+	MaterialPasses.Empty();
+	SetupRenderPasses();
 }
 
 void UAGX_CameraSensorComponent::UpdateNativeTransform()
@@ -34,6 +110,56 @@ USceneCaptureComponent2D* UAGX_CameraSensorComponent::GetSceneCaptureComponent2D
 bool UAGX_CameraSensorComponent::IsCameraSensorValid() const
 {
 	return CaptureComponent2D != nullptr && HasNative();
+}
+
+UTextureRenderTarget2D* UAGX_CameraSensorComponent::RenderCameraPipeline()
+{
+	if (CaptureComponent2D == nullptr)
+		return nullptr;
+
+	EnsureRenderTargets();
+	if (SceneRenderTarget == nullptr)
+		return nullptr;
+
+	CaptureComponent2D->CaptureScene();
+
+	UTexture* InputTexture = SceneRenderTarget;
+	UTextureRenderTarget2D* FinalRenderTarget = SceneRenderTarget;
+	for (int32 Index = 0; Index < MaterialInstances.Num(); ++Index)
+	{
+		UMaterialInstanceDynamic* MaterialInstance = MaterialInstances[Index];
+		if (MaterialInstance == nullptr)
+			continue;
+
+		if (!RenderTargets.IsValidIndex(Index) || RenderTargets[Index] == nullptr)
+			continue;
+
+		UTextureRenderTarget2D* OutputRenderTarget = RenderTargets[Index].Get();
+		MaterialInstance->SetTextureParameterValue(MaterialInputTextureParameterName, InputTexture);
+
+		UCanvas* Canvas = nullptr;
+		FVector2D CanvasSize(0.0f, 0.0f);
+		FDrawToRenderTargetContext DrawContext;
+		UKismetRenderingLibrary::BeginDrawCanvasToRenderTarget(
+			this, OutputRenderTarget, Canvas, CanvasSize, DrawContext);
+
+		if (Canvas != nullptr)
+		{
+			FCanvasTileItem Item(
+				FVector2D::ZeroVector, MaterialInstance->GetRenderProxy(),
+				FVector2D(Resolution.X, Resolution.Y), FVector2D(0.0f, 0.0f),
+				FVector2D(1.0f, 1.0f));
+			Item.BlendMode = SE_BLEND_Opaque;
+			Canvas->DrawItem(Item);
+		}
+
+		UKismetRenderingLibrary::EndDrawCanvasToRenderTarget(this, DrawContext);
+
+		FinalRenderTarget = OutputRenderTarget;
+		InputTexture = FinalRenderTarget;
+	}
+
+	return FinalRenderTarget;
 }
 
 FSensorBarrier* UAGX_CameraSensorComponent::CreateNativeImpl()
@@ -84,6 +210,7 @@ void UAGX_CameraSensorComponent::BeginPlay()
 	if (HasNative())
 	{
 		SetupSceneCapture();
+		SetupRenderPasses();
 
 		if (auto Se = UAGX_SensorEnvironmentSubsystem::GetFrom(this))
 		{
@@ -117,6 +244,7 @@ void UAGX_CameraSensorComponent::PostApplyToComponent()
 		// Dynamic Components are not carried over when a Blueprint instance is reconstructed
 		// during Play, so recreate the runtime Scene Capture Component on the new instance.
 		SetupSceneCapture();
+		SetupRenderPasses();
 	}
 }
 
@@ -129,7 +257,26 @@ void UAGX_CameraSensorComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
 		CaptureComponent2D->DestroyComponent();
 		CaptureComponent2D = nullptr;
 	}
+
+	MaterialInstances.Empty();
+	RenderTargets.Empty();
+	SceneRenderTarget = nullptr;
 }
+
+#if WITH_EDITOR
+void UAGX_CameraSensorComponent::PostEditChangeChainProperty(FPropertyChangedChainEvent& Event)
+{
+	FAGX_PropertyChangedDispatcher<ThisClass>::Get().Trigger(Event);
+
+	Super::PostEditChangeChainProperty(Event);
+}
+
+void UAGX_CameraSensorComponent::PostInitProperties()
+{
+	Super::PostInitProperties();
+	InitPropertyDispatcher();
+}
+#endif
 
 FCameraBarrier* UAGX_CameraSensorComponent::GetNativeAsCamera()
 {
@@ -180,6 +327,90 @@ void UAGX_CameraSensorComponent::SetupSceneCapture()
 	CaptureComponent2D = NewObject<USceneCaptureComponent2D>(Owner, NAME_None);
 	CaptureComponent2D->SetupAttachment(this);
 	CaptureComponent2D->SetCanEverAffectNavigation(false);
+	CaptureComponent2D->bCaptureEveryFrame = false;
+	CaptureComponent2D->bCaptureOnMovement = false;
+	CaptureComponent2D->bAlwaysPersistRenderingState = true;
 	CaptureComponent2D->CreationMethod = CreationMethod;
 	CaptureComponent2D->RegisterComponentWithWorld(World);
 }
+
+void UAGX_CameraSensorComponent::SetupRenderPasses()
+{
+	MaterialInstances.Empty();
+
+	if (CaptureComponent2D == nullptr)
+		return;
+
+	if (!IsResolutionValid(Resolution))
+		return;
+
+	for (const TObjectPtr<UMaterialInterface>& Material : MaterialPasses)
+	{
+		if (Material == nullptr)
+			continue;
+
+		MaterialInstances.Add(UMaterialInstanceDynamic::Create(Material.Get(), this));
+	}
+
+	EnsureRenderTargets();
+}
+
+void UAGX_CameraSensorComponent::EnsureRenderTargets()
+{
+	if (CaptureComponent2D == nullptr)
+		return;
+
+	if (!IsResolutionValid(Resolution))
+		return;
+
+	if (!IsRenderTargetUpToDate(SceneRenderTarget))
+		SceneRenderTarget = CreateRenderTarget();
+
+	CaptureComponent2D->TextureTarget = SceneRenderTarget;
+
+	if (RenderTargets.Num() < MaterialInstances.Num())
+		RenderTargets.SetNum(MaterialInstances.Num());
+
+	for (int32 Index = 0; Index < MaterialInstances.Num(); ++Index)
+	{
+		if (!IsRenderTargetUpToDate(RenderTargets[Index]))
+			RenderTargets[Index] = CreateRenderTarget();
+	}
+}
+
+UTextureRenderTarget2D* UAGX_CameraSensorComponent::CreateRenderTarget()
+{
+	UTextureRenderTarget2D* RenderTarget = NewObject<UTextureRenderTarget2D>(this);
+	RenderTarget->bGPUSharedFlag = true;
+	RenderTarget->RenderTargetFormat = ETextureRenderTargetFormat::RTF_RGBA8;
+	RenderTarget->TargetGamma = 2.2f;
+	RenderTarget->InitAutoFormat(Resolution.X, Resolution.Y);
+	return RenderTarget;
+}
+
+bool UAGX_CameraSensorComponent::IsRenderTargetUpToDate(
+	const UTextureRenderTarget2D* RenderTarget) const
+{
+	return RenderTarget != nullptr && RenderTarget->SizeX == Resolution.X &&
+		   RenderTarget->SizeY == Resolution.Y;
+}
+
+bool UAGX_CameraSensorComponent::IsResolutionValid(const FIntPoint& InResolution)
+{
+	return InResolution.X >= 1 && InResolution.Y >= 1;
+}
+
+#if WITH_EDITOR
+void UAGX_CameraSensorComponent::InitPropertyDispatcher()
+{
+	FAGX_PropertyChangedDispatcher<ThisClass>& PropertyDispatcher =
+		FAGX_PropertyChangedDispatcher<ThisClass>::Get();
+	if (PropertyDispatcher.IsInitialized())
+		return;
+
+	AGX_COMPONENT_DEFAULT_DISPATCHER(Resolution);
+	AGX_COMPONENT_DEFAULT_DISPATCHER(MaterialInputTextureParameterName);
+	PropertyDispatcher.Add(
+		AGX_MEMBER_NAME(MaterialPasses), [](ThisClass* This) { This->SetupRenderPasses(); });
+}
+#endif
--- Source/AGXUnreal/Public/Sensors/AGX_CameraSensorComponent.h
+++ Source/AGXUnreal/Public/Sensors/AGX_CameraSensorComponent.h
@@ -8,7 +8,10 @@
 #include "AGX_CameraSensorComponent.generated.h"
 
 struct FCameraBarrier;
+class UMaterialInstanceDynamic;
+class UMaterialInterface;
 class USceneCaptureComponent2D;
+class UTextureRenderTarget2D;
 
 /**
  * Todo: add API comment.
@@ -23,6 +26,52 @@ class AGXUNREAL_API UAGX_CameraSensorComponent : public UAGX_SensorComponentBase
 
 public:
 	UAGX_CameraSensorComponent();
+
+	/**
+	 * Output resolution of the Camera Sensor [pixels].
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AGX Camera", Meta = (ClampMin = "1"))
+	FIntPoint Resolution {256, 256};
+
+	/**
+	 * Set the output resolution of the Camera Sensor.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "AGX Camera")
+	void SetResolution(FIntPoint InResolution);
+
+	/**
+	 * Name of the texture parameter that receives the previous render pass output.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AGX Camera")
+	FName MaterialInputTextureParameterName {TEXT("InputTexture")};
+
+	/**
+	 * Set the name of the texture parameter that receives the previous render pass output.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "AGX Camera")
+	void SetMaterialInputTextureParameterName(FName InParameterName);
+
+	/**
+	 * Materials used as full-screen render passes after the Scene Capture Component has captured
+	 * the scene.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AGX Camera")
+	TArray<TObjectPtr<UMaterialInterface>> MaterialPasses;
+
+	UFUNCTION(BlueprintCallable, Category = "AGX Camera")
+	void AddMaterialPass(UMaterialInterface* Material);
+
+	UFUNCTION(BlueprintCallable, Category = "AGX Camera")
+	bool SetMaterialPass(int32 Index, UMaterialInterface* Material);
+
+	UFUNCTION(BlueprintCallable, Category = "AGX Camera")
+	bool RemoveMaterialPass(UMaterialInterface* Material);
+
+	UFUNCTION(BlueprintCallable, Category = "AGX Camera")
+	bool RemoveMaterialPassAt(int32 Index);
+
+	UFUNCTION(BlueprintCallable, Category = "AGX Camera")
+	void ClearMaterialPasses();
 
 	void UpdateNativeTransform();
 
@@ -42,12 +91,22 @@ public:
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "AGX Camera")
 	bool IsCameraSensorValid() const;
 
+	UFUNCTION(BlueprintCallable, Category = "AGX Camera")
+	UTextureRenderTarget2D* RenderCameraPipeline();
+
 	//~ Begin UActorComponent Interface
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type Reason) override;
 	virtual void PostApplyToComponent() override;
 	virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
 	//~ End UActorComponent Interface
+
+	//~ Begin UObject Interface
+#if WITH_EDITOR
+	virtual void PostEditChangeChainProperty(FPropertyChangedChainEvent& Event) override;
+	virtual void PostInitProperties() override;
+#endif
+	//~ End UObject Interface
 
 	FCameraBarrier* GetNativeAsCamera();
 	const FCameraBarrier* GetNativeAsCamera() const;
@@ -59,7 +118,25 @@ private:
 	virtual void UpdateNativeProperties() override;
 
 	void SetupSceneCapture();
+	void SetupRenderPasses();
+	void EnsureRenderTargets();
+	UTextureRenderTarget2D* CreateRenderTarget();
+	bool IsRenderTargetUpToDate(const UTextureRenderTarget2D* RenderTarget) const;
+	static bool IsResolutionValid(const FIntPoint& InResolution);
+
+#if WITH_EDITOR
+	void InitPropertyDispatcher();
+#endif
 
 	UPROPERTY(Transient)
 	TObjectPtr<USceneCaptureComponent2D> CaptureComponent2D {nullptr};
+
+	UPROPERTY(Transient)
+	TObjectPtr<UTextureRenderTarget2D> SceneRenderTarget {nullptr};
+
+	UPROPERTY(Transient)
+	TArray<TObjectPtr<UTextureRenderTarget2D>> RenderTargets;
+
+	UPROPERTY(Transient)
+	TArray<TObjectPtr<UMaterialInstanceDynamic>> MaterialInstances;
 };
