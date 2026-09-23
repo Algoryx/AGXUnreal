6ea5340448d22736d61276139418d1eb1b3961a6
Josef <josef.holmner@algoryx.com>
2026-09-23 08:18:07 +0200

    Ensure we free the scene name on the agx side

--- Source/AGXUnrealBarrier/Private/Import/AGXSimObjectsReader.cpp
+++ Source/AGXUnrealBarrier/Private/Import/AGXSimObjectsReader.cpp
@@ -46,6 +46,7 @@
 #include <agxCable/Cable.h>
 #include <agxTerrain/TerrainWheel.h>
 #include <agxTerrain/Utils.h>
+#include <agxUtil/agxUtil.h>
 
 // In 2.28 including Cable.h causes a preprocessor macro named DEPRECATED to be defined. This
 // conflicts with a macro with the same name in Unreal. Undeffing the Unreal one.
@@ -792,6 +793,9 @@ bool FAGXSimObjectsReader::ReadOpenPLXFile(
 	OutSimObjects.GetOpenPLXInputs() = FPLXUtilitiesInternal::GetInputs(System.get());
 	OutSimObjects.GetOpenPLXOutputs() = FPLXUtilitiesInternal::GetOutputs(System.get());
 
-	OutSimObjects.SetModelName(Convert(Result.scene()->getType()->getNameWithNamespace(".")));
+	std::string SceneNamePLX = Result.scene()->getType()->getNameWithNamespace(".");
+	OutSimObjects.SetModelName(Convert(SceneNamePLX));
+	agxUtil::freeContainerMemory(SceneNamePLX);
+
 	return true;
 }
